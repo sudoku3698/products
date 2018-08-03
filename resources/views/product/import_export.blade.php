618c59eae340052d6c958eb4a00406ffdaf9081d
sudoku3698 <sud.jad3698@gmail.com>
2018-08-03 17:44:44 +0530

    first commit

--- resources/views/product/import_export.blade.php
+++ resources/views/product/import_export.blade.php
@@ -10,17 +10,14 @@
                 <div class="card-body">
                 	@if(Session::has('error'))
                	    <div class="alert alert-danger">
-				      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				        <span aria-hidden="true">×</span>
+
 				      </button>
 				      <strong>Failed!</strong> {{ Session::get('error') }} 
 				    </div>
 				    @endif
 				    @if(Session::has('errors'))
 				    <div class="alert alert-danger">
-				      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				        <span aria-hidden="true">×</span>
-				      </button>
+
 				       <strong>Failed!</strong> 
 					 	 <ul>
 					      <?php foreach(Session::get('errors') as $row=>$err){ ?> 
@@ -42,20 +39,18 @@
 				    @endif  
                     @if(Session::has('success'))
 					<div class="alert alert-success">
-				      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				        <span aria-hidden="true">×</span>
 				      </button>
 				      <strong>Success!</strong> {{ Session::get('success')}}
 				    </div>
 					@endif
 				<br />
-				<a href="{{ URL::to('downloadExcel/xls') }}"><button class="btn btn-success">Download Excel xls</button></a>
-				<a href="{{ URL::to('downloadExcel/xlsx') }}"><button class="btn btn-success">Download Excel xlsx</button></a>
-				<a href="{{ URL::to('downloadExcel/csv') }}"><button class="btn btn-success">Download CSV</button></a>
-				<form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="{{ URL::to('importExcel') }}" class="form-horizontal" method="post" enctype="multipart/form-data">
+				<a href="{{ URL::to('downloadExcel/xls') }}"><button >Download Excel xls</button></a>
+				<a href="{{ URL::to('downloadExcel/xlsx') }}"><button c>Download Excel xlsx</button></a>
+				<a href="{{ URL::to('downloadExcel/csv') }}"><button >Download CSV</button></a>
+				<form  action="{{ URL::to('importExcel') }}" class="form-horizontal" method="post" enctype="multipart/form-data">
 					{{ csrf_field() }}
 					<input type="file" name="import_file" />
-					<button class="btn btn-primary">Import File</button>
+					<button >Import File</button>
 				</form>
                     
                 </div>
