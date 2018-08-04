1b24d6c175d14fc1bc9ecfc6f598e19cf2bd09ba
sudoku3698 <sud.jad3698@gmail.com>
2018-08-05 00:05:09 +0530

    optimise insert and update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,8 @@ use App\Product;
 use DB;
 use Session;
 use Excel;
+use App\Http\Requests\StoreProductsValdation;
+
 
 class ProductController extends Controller
 {
@@ -31,7 +33,7 @@ class ProductController extends Controller
 
             $excel->sheet('mySheet', function($sheet)
             {
-            	$product=Product::all();
+            	//$product=Product::all();
             	$reload=array();
                 $reload[] = array(
                 	'product_name' => "", 
@@ -61,59 +63,57 @@ class ProductController extends Controller
     	$inserted_count=0;
     	$updated_count=0;
     	$final_error=array();
+        $product_data=array();
     	
         if($request->hasFile('import_file'))
         {
 
-            Excel::load($request->file('import_file')->getRealPath(), function ($reader) use (&$inserted_count,&$updated_count,&$final_error) {
+            Excel::load($request->file('import_file')->getRealPath(), function ($reader) use (&$inserted_count,&$updated_count,&$final_error,&$product_data) {
                 foreach ($reader->toArray() as $key => $row) 
                 {
 
                 	//Validation Check For Each Row
-                    $validator = \Validator::make($row, [
-					            'product_name' => 'required',
-					            'product_url' => 'required|regex:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/',
-					            'product_sku' => 'required',
-					            'product_description' => 'required',
-					            'product_color' => 'required',
-					            'product_size' => 'required',
-					            'product_uuid' => 'required'
-					            //'product_uuid' => 'required|unique:products,product_uuid'
-					        ]);
+                    $validator = \Validator::make($row,(new StoreProductsValdation)->rules());
 					  if($validator->fails()) 
 					  {
 					  	$final_error[$key]=$validator->errors()->messages();
 					  }else
 					  {	
+
+                        $product_data[]=$row;
 					  	//check if uuid is present or not if it is so then update orelse save
-					  	$product=Product::where(['product_uuid'=>$row['product_uuid']])->first();
-					  	if($product)
-					  	{
-					  		$product->product_name=$row['product_name'];
-					  		$product->product_url=$row['product_url'];
-					  		$product->product_sku=$row['product_sku'];
-					  		$product->product_description=$row['product_description'];
-					  		$product->product_color=$row['product_color'];
-					  		$product->product_size=$row['product_size'];
-					  		$product->save();
-					  		$updated_count++;
-					  	}else
-					  	{
-					  		$product=new Product();
-						  	$product->product_name=$row['product_name'];
-					  		$product->product_url=$row['product_url'];
-					  		$product->product_sku=$row['product_sku'];
-					  		$product->product_description=$row['product_description'];
-					  		$product->product_color=$row['product_color'];
-					  		$product->product_size=$row['product_size'];
-					  		$product->product_uuid=$row['product_uuid'];
-					  		$product->save();
-					  		$inserted_count++;
-					  	}
+					  	// $product=Product::where(['product_uuid'=>$row['product_uuid']])->first();
+					  	// if($product)
+					  	// {
+					  	// 	$product->product_name=$row['product_name'];
+					  	// 	$product->product_url=$row['product_url'];
+					  	// 	$product->product_sku=$row['product_sku'];
+					  	// 	$product->product_description=$row['product_description'];
+					  	// 	$product->product_color=$row['product_color'];
+					  	// 	$product->product_size=$row['product_size'];
+					  	// 	$product->save();
+					  	// 	$updated_count++;
+					  	// }else
+					  	// {
+					  	// 	$product=new Product();
+						  // 	$product->product_name=$row['product_name'];
+					  	// 	$product->product_url=$row['product_url'];
+					  	// 	$product->product_sku=$row['product_sku'];
+					  	// 	$product->product_description=$row['product_description'];
+					  	// 	$product->product_color=$row['product_color'];
+					  	// 	$product->product_size=$row['product_size'];
+					  	// 	$product->product_uuid=$row['product_uuid'];
+					  	// 	$product->save();
+					  	// 	$inserted_count++;
+					  	// }
 					  }	
                 }
                 
             });
+            //ON DUPLICATE KEY UPDATE product_uuid=values(product_uuid)
+            $Product_result=Product::insertOnDuplicateKey($product_data, ['product_uuid','product_color','product_name','product_url','product_sku','product_description','product_size']);
+            print_r($Product_result);
+            exit;
         }
         if($inserted_count>0 || $updated_count>0)
 		        {
---
+++ app/Http/Requests/StoreProductsValdation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductsValdation extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'product_name' => 'required',
+            'product_url' => 'required|regex:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/',
+            'product_sku' => 'required',
+            'product_description' => 'required',
+            'product_color' => 'required',
+            'product_size' => 'required',
+            'product_uuid' => 'required'
+        ];
+    }
+}
