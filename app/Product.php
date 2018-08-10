c4a28182f2d761e234a9df5b2c959e5c7334337b
sudoku3698 <sud.jad3698@gmail.com>
2018-08-10 20:00:52 +0530

    product data import in chuncks

--- app/Product.php
+++ app/Product.php
@@ -14,6 +14,8 @@ class Product extends Model
 	//table name from database
     protected $table="products";
 
+    static $master_product_data=array();
+
     protected $fillable = ['product_name', 'product_url','product_sku','product_description','product_color','product_size','product_uuid'];
 
 
@@ -47,7 +49,7 @@ class Product extends Model
      */
     public static function importProductData($file)
     {
-        //dd($file);
+
         $extention="";
         $product_data=array();
         $response_data=array();
@@ -66,23 +68,15 @@ class Product extends Model
         if(in_array($extention, self::allowedFileExtentionsForProductImport()))
         {
             $result=self::load_excel_data($file);
+            $response_data['Product_result']=$result['Product_result'];
             $response_data['final_error']=$result['final_error'];
-            $product_data=$result['product_data'];
-            if(!empty($product_data))
-            {
-                $response_data['Product_result']=Product::InsertOrUpdateInBulk($product_data);
-            }else
-            {
-                $response_data['error']="Empty data in excel sheet";
-            }
-
-            return $response_data;
         }else
         {
             //dd('Invalid File');
             $response_data['error']="Not a valid file";
-            return $response_data;
+            
         }
+        return $response_data;
     }
 
 
@@ -96,12 +90,13 @@ class Product extends Model
      */
     public static function load_excel_data($get_file_path)
     { 
-
         $extention="";
         $result=array();
         $result['product_data']=array();
         $result['final_error']=array();
         $info=pathinfo($get_file_path);
+        $count=0;
+        $co=0;
         if(isset($info['extension']))
         {
             $extention=$info['extension'];
@@ -112,45 +107,25 @@ class Product extends Model
         if(in_array($extention, self::allowedFileExtentionsForProductImport()))
         {
             $get_file_path=is_object($get_file_path)?$get_file_path->getRealPath():$get_file_path;
-            Excel::load($get_file_path, function ($reader) use (&$inserted_count,&$updated_count,&$final_error,&$product_data,&$result) {
-                    //return errors array and valid product_data array
-                    $result=self::validOrInvalidProductDataRequest($reader->toArray());
+            Excel::load($get_file_path, function ($reader) use (&$inserted_count,&$updated_count,&$final_error,&$product_data,&$result,&$count,&$co) {
+
+                //return errors array and valid product_data array
+                $result=self::validOrInvalidProductDataRequest($reader->toArray());
+
+                //import data in chunk into the database
+                foreach(array_chunk($result['product_data'], 500) as $value){
+                   $co=self::insertOnDuplicateKey($value, ['product_color','product_name','product_url','product_sku','product_description','product_size']);
+                   $count=$count+$co;
+                  }
                 });
-            return $result;
+            $result['Product_result']=$count;
         }else
         {
             $result['error']="Not a valid file";
         }
+        unset($result['product_data']);
+        return $result;
     }
-
-    //insert and update product data in bulk
-    /**
-     * import excel file
-     *
-     * @param  product data array
-     * 
-     * @return affected rows or false
-     */
-     public static function InsertOrUpdateInBulk($product_data)
-     {
-     	/*ON DUPLICATE KEY UPDATE product_color=values(product_color),product_name=values(product_name),product_url=values(product_url),product_sku=values(product_sku),product_description=values(product_description),product_size=values(product_size)
-
-        INSERT INTO `products`(`product_name`,`product_url`,`product_sku`,`product_description`,`product_color`,`product_size`,`product_uuid`) VALUES\n
-        (?,?,?,?,?,?,?), (?,?,?,?,?,?,?)\n
-        ON DUPLICATE KEY UPDATE `product_color` = VALUES(`product_color`), `product_name` = VALUES(`product_name`), `product_url` = VALUES(`product_url`), `product_sku` = VALUES(`product_sku`), `product_description` = VALUES(`product_description`), `product_size` = VALUES(`product_size`)
-        returns 1 on insert and 2 on update 
-        */
-        if($product_data)
-        {
-            foreach (array_chunk($product_data, 5000) as $idListBatch) {
-                ImportProductsToDatabase::dispatch($idListBatch)->delay(now()->addSecond(2));
-            }
-           // return self::insertOnDuplicateKey($product_data, ['product_color','product_name','product_url','product_sku','product_description','product_size']);
-             return true;
-        }
-        return false;
-     	
-     }
 
     //check valid and invalid data
     /**
@@ -178,5 +153,36 @@ class Product extends Model
         }
 
         return array('final_error'=>$final_error,'product_data'=>$product_data_array);
+     }
+
+      //insert and update product data in bulk
+    /**
+     * import excel file
+     *
+     * @param  product data array
+     * 
+     * @return affected rows or false
+     */
+     public static function InsertOrUpdateInBulk($product_data)
+     {
+        /*ON DUPLICATE KEY UPDATE product_color=values(product_color),product_name=values(product_name),product_url=values(product_url),product_sku=values(product_sku),product_description=values(product_description),product_size=values(product_size)
+
+        INSERT INTO `products`(`product_name`,`product_url`,`product_sku`,`product_description`,`product_color`,`product_size`,`product_uuid`) VALUES\n
+        (?,?,?,?,?,?,?), (?,?,?,?,?,?,?)\n
+        ON DUPLICATE KEY UPDATE `product_color` = VALUES(`product_color`), `product_name` = VALUES(`product_name`), `product_url` = VALUES(`product_url`), `product_sku` = VALUES(`product_sku`), `product_description` = VALUES(`product_description`), `product_size` = VALUES(`product_size`)
+        returns 1 on insert and 2 on update 
+        */
+        if($product_data)
+        {
+            $result=0;
+            foreach (array_chunk($product_data, 5000) as $idListBatch) {
+                //ImportProductsToDatabase::dispatch($idListBatch)->delay(now()->addSecond(2));
+                $result=$result+self::insertOnDuplicateKey($product_data, ['product_color','product_name','product_url','product_sku','product_description','product_size']);
+            }
+           // return self::insertOnDuplicateKey($product_data, ['product_color','product_name','product_url','product_sku','product_description','product_size']);
+             return $result;
+        }
+        return false;
+        
      }
 }
